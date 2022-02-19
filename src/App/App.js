fc3c044236f706fc5d2d1ae1337f204c619753f2
aaajii <jwpbanawan@gmail.com>
2022-02-19 22:40:53 +0800

    Add sample chart

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import SampleChart from '../components/SampleChart'
+import { TVChartContainer } from '../components/TVChart'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>(Keeping this logo because it deffo looks cool)</p>
       </header>
+
+      <hr />
+
+      <h1> Sample Chart from Kaktana</h1>
+      <SampleChart />
+
+      {/* <h1> TVChart that i've stolen online hehe</h1>
+      <TVChartContainer token={''} /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
